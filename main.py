b82165a720fd5452fbb5748372bb562095fa74fe
Yashenyka <62373072+Yashenyka@users.noreply.github.com>
2021-02-23 13:49:42 +0500

    first

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def draw(self, qp):
+        x = random.randint(10, 300)
+        y = random.randint(10, 300)
+        w = random.randint(10, 85)
+        h = random.randint(10, 85)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, w, h)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
